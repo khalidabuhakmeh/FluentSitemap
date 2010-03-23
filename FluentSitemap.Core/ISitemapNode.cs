0d0eeb848298bcf40db491e45a740395a511fba8
Khalid Abuhakmeh <Khalid@AquaBirdConsulting.com>
2010-03-22 22:02:45 -0400

    ignoring sitemap.xml.

--- FluentSitemap.Core/ISitemapNode.cs
+++ FluentSitemap.Core/ISitemapNode.cs
@@ -1,8 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Web;
+﻿// Copyright 2010 Aqua Bird Consulting (http://www.aquabirdconsulting.com)
+// 
+// Licensed under the Apache License, Version 2.0 (the "License"); 
+// you may not use this file except in compliance with the License. 
+// You may obtain a copy of the License at 
+// 
+// http://www.apache.org/licenses/LICENSE-2.0 
+// 
+// Unless required by applicable law or agreed to in writing, software 
+// distributed under the License is distributed on an "AS IS" BASIS, 
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+// See the License for the specific language governing permissions and 
+// limitations under the License.
+// 
+// The latest version of this file can be found at http://github.com/khalidabuhakmeh/FluentSitemap
+using System;
+
 
 namespace FluentSitemap.Core
 {
@@ -19,10 +31,5 @@ namespace FluentSitemap.Core
         ISitemapNode WithPriority(double? priority);
         ISitemap Set(ISitemapNode changes);
         ISitemap Set();
-    }
-
-    public enum ChangeFrequencyType
-    {
-        Always, Hourly, Daily, Weekly, Monthly, Yearly, Never
     }
 }
