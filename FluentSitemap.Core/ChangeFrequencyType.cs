e853761359d3dbde6b3d811c902895f507c74f7d
Khalid Abuhakmeh <Khalid@AquaBirdConsulting.com>
2010-03-22 21:18:28 -0400

    added Apache 2.0 license and readme

--- FluentSitemap.Core/ChangeFrequencyType.cs
+++ FluentSitemap.Core/ChangeFrequencyType.cs
@@ -13,22 +13,10 @@
 // limitations under the License.
 // 
 // The latest version of this file can be found at http://github.com/khalidabuhakmeh/FluentSitemap
-using System;
-
 namespace FluentSitemap.Core
 {
-    public static class SitemapNodeValidator
+    public enum ChangeFrequencyType
     {
-        public static void Validate(ISitemapNode node)
-        {
-            if (node == null) 
-                throw new ArgumentNullException("node");
-
-            if (string.IsNullOrWhiteSpace(node.Location))
-                throw new ArgumentNullException("node.Location", "Location is required.");
-
-            if (node.Priority.HasValue && (node.Priority < 0 || node.Priority > 1))
-                throw new ArgumentNullException("node.Priority", "Priority must be between 0.0 and 1.0.");
-        }
+        Always, Hourly, Daily, Weekly, Monthly, Yearly, Never
     }
 }
---
+++ readme.txt
@@ -0,0 +1,15 @@
+FluentSitemap (c) Aqua Bird Consulting
+http://www.aquabirdconsulting.com
+
+Author : Khalid Abuhakmeh
+
+A small sitemap library for Asp.Net MVC that uses a fluent
+interface to build a sitemap used by search engines to index
+a web site.
+
+This project is licensed under the Apache License 2.0 
+http://www.apache.org/licenses/LICENSE-2.0.html
+
+For documentation, binary realeases, and discussions please visit the GitHub site:
+
+http://github.com/khalidabuhakmeh/FluentSitemap
